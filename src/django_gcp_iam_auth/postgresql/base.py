69124635698755eac8bd27679d0a9d4eb0f349c6
Rob Clevenger <rob@robsite.org>
2024-03-06 15:19:36 -0800

    remove need to downscope since the ADC token is requested with the right scopes

--- src/django_gcp_iam_auth/postgresql/base.py
+++ src/django_gcp_iam_auth/postgresql/base.py
@@ -16,39 +16,15 @@ class DatabaseWrapper(base.DatabaseWrapper):
     def get_connection_params(self):
         params = super().get_connection_params()
         # need to remove this otherwise we'll get errors like
-        # invalid dsn: invalid connection option "gcp_iam_auth"
+        #   'invalid dsn: invalid connection option "gcp_iam_auth"'
         if params.pop("gcp_iam_auth", None):
-
-            # TODO - check if _credentials already exists and the token is still valid
-            # refreshing if needed
             self._credentials, _ = google.auth.default(scopes=CLOUDSQL_IAM_LOGIN_SCOPE)
             if not self._credentials.token_state == TokenState.FRESH:
-                print('** Refreshing token')
                 self._credentials.refresh(requests.Request())
             params.setdefault("port", 5432)
-            # Set password to newly fetched oauth token
+            # TODO - should we add in a resource restriction for the DB instance?
+            # https://cloud.google.com/iam/docs/downscoping-short-lived-credentials#auth_downscoping_token_broker-python
+            # Set password to newly fetched access token
             params["password"] = self._credentials.token
-            print('** USING GCP IAM TOKEN FOR PASSWORD')
 
         return params
-    
-
-def _downscope_credentials(
-    credentials: Credentials,
-    ) -> Credentials:
-
-    # credentials sourced from a service account or metadata are children of
-    # Scoped class and are capable of being re-scoped
-    if isinstance(credentials, Scoped):
-        scoped_creds = credentials.with_scopes(scopes=CLOUDSQL_IAM_LOGIN_SCOPE)
-    # authenticated user credentials can not be re-scoped
-    else:
-        # create shallow copy to not overwrite scopes on default credentials
-        scoped_creds = copy.copy(credentials)
-        # overwrite '_scopes' to down-scope user credentials
-        # Cloud SDK reference: https://github.com/google-cloud-sdk-unofficial/google-cloud-sdk/blob/93920ccb6d2cce0fe6d1ce841e9e33410551d66b/lib/googlecloudsdk/command_lib/sql/generate_login_token_util.py#L116
-        scoped_creds._scopes = CLOUDSQL_IAM_LOGIN_SCOPE
-    # down-scoped credentials require refresh, are invalid after being re-scoped
-    request = google.auth.transport.requests.Request()
-    scoped_creds.refresh(request)
-    return scoped_creds
